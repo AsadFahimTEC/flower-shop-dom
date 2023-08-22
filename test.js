dd4dd2ebce939aeb01b9e81c03445b7d2b935890
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-08-22 14:27:39 +0600

    create list items and appendchild with itemName shown in right side

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
 function handleCLikBtn(target){
-    const itemName = target.parentNode.childNodes[3].innerText;
-    console.log(itemName);
+    const itemName = target.parentNode.childNodes[1].innerText;
+    // get reference of selected items
+    const selectedItems = document.getElementById('selected-items');
+
+    // create an elements
+    const li = document.createElement('li');
+    li.innerText = itemName;
+    selectedItems.appendChild(li);
+
 }
