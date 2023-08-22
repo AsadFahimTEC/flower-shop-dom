b9228e067482ff96044fa22ebb8c60a999ee60ab
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-08-22 13:46:05 +0600

    onclick this event with child and parent nodes class finish

--- test2.js
+++ test2.js
@@ -1,27 +1,33 @@
 // common function call to get innerText with id and text
-function test(params){
-    setText('info1', 'Bangladesh');
-    const sum = toNumber('input1');
-    console.log(sum);
-    }
+// function test(params){
+//     setText('info1', 'Bangladesh');
+//     const sum = toNumber('input1');
+//     console.log(sum);
+//     }
     
-    function test2(params){
-    setText('info2', 'Bangladesh');
-    const sum2 = toNumber('input1');
-    console.log(sum2);
-    }
+    // function test2(params){
+    // setText('info2', 'Bangladesh');
+    // const sum2 = toNumber('input1');
+    // console.log(sum2);
+    // }
     
-    function test3(params){
-    setText('info3', 'Bangladesh');
-    }
+    // function test3(params){
+    // setText('info3', 'Bangladesh');
+    // }
     
-    // common function call with innerText
-    function setText(id, text){
-        document.getElementById(id).innerText = text;
-    }
+    // // common function call with innerText
+    // function setText(id, text){
+    //     document.getElementById(id).innerText = text;
+    // }
     
-    // function call to convert input into number and return value
-    function toNumber(id){
-    const result = document.getElementById(id).value;
-    return parseInt(result);
-    }
+    // // function call to convert input into number and return value
+    // function toNumber(id){
+    // const result = document.getElementById(id).value;
+    // return parseInt(result);
+    // }
+
+    // specific this for any button click with found parent and child nodes with index position of any elements with innerText
+    // function handleClick(data){
+    //     console.log(data.parentNode.childNodes[1].innerText);
+    // }
+
