bdae9ac6a459b47851f1d8c16d3b08d07058a9b2
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-08-22 15:03:09 +0600

    total calculation complete

--- test.js
+++ test.js
@@ -1,11 +1,22 @@
+// intialized global  variable
+let total = 0;
+
 function handleCLikBtn(target){
+      // get reference of selected items
+      const selectedItems = document.getElementById('selected-items');
+
+    // show the item names innerText
     const itemName = target.parentNode.childNodes[1].innerText;
-    // get reference of selected items
-    const selectedItems = document.getElementById('selected-items');
 
     // create an elements
     const li = document.createElement('li');
     li.innerText = itemName;
     selectedItems.appendChild(li);
 
+    const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
+
+    // calculate the total
+    total = parseInt(total) + parseInt(price);
+    console.log(total);
+    document.getElementById('total').innerText= total;
 }
